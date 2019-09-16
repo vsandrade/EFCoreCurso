e5161d9f20d54db81ffaffe89d40a89ffa17bb0a
Vinícius <ozirispc@gmail.com>
2019-09-16 12:34:58 -0300

    Logging e Métodos de Filtro

--- EFCore.WebAPI/Controllers/ValuesController.cs
+++ EFCore.WebAPI/Controllers/ValuesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using EFCore.Dominio;
 using EFCore.Repo;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore.WebAPI.Controllers
 {
@@ -22,9 +23,10 @@ namespace EFCore.WebAPI.Controllers
         public ActionResult GetFiltro(string nome)
         {
             var listHeroi = _context.Herois
-                            .Where(h => h.Nome.Contains(nome))
-                            .ToList();
-            
+                            .Where(h => EF.Functions.Like(h.Nome, $"%{nome}%"))
+                            .OrderBy(h => h.Id)
+                            .LastOrDefault();
+
             //var listHeroi = (from heroi in _context.Herois
             //                 where heroi.Nome.Contains(nome)
             //                 select heroi).ToList();
